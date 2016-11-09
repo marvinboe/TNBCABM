e99cbf8d81cf2bce4e8280ca958fd34a91b1df0b
Marvin Boettcher <boettcher@evolbio.mpg.de>
2016-11-09 11:11:11 -0500

    minor changes.

--- src/kernel.cpp
+++ src/kernel.cpp
@@ -2,18 +2,23 @@
 #include "kernel.h"
 
 
-
-
 std::uniform_real_distribution<double> Kernel::uniform01(0.,1.);
 
 double Kernel::direct_update(double t){
 
     double tau=1./_all_reactions.rate_sum() * std::log(uniform01(rng));
 
-    Reaction* reaction= _all_reactions[0];
-    
-    reaction->apply(model);
+    double randchoice=_all_reactions.rate_sum() * uniform01(rng);
 
+    double weightsum=0.;
+    int i=0;
+    while (weightsum<randchoice){
+        weightsum+=_all_reactions[i]->rate();
+        ++i;
+    }
+    Reaction* reaction= _all_reactions[i];
+
+    reaction->apply(model);
 
     return t+tau;
 }
