66b2f3d2629df10d3ddd15a7da8723ddfce0699a
WeiniHuang <huang.weini@gmail.com>
2016-11-09 14:32:23 +0000

    add functions  Please enter the commit message for your changes. Lines starting

--- include/model.h
+++ include/model.h
@@ -6,19 +6,23 @@
 
 class Model{
     public:
-        Model();
-
-        double get_Ccell_number(int comp, int type);
-        void set_Ccell_number(int type,int comp, double number);
+       
+        
+		Model(int initial_pt_size,int initial_mt_size,double num_micromets,double initial_pt_birthRate,double initial_mt_birthRate);
+			
+        double get_Ccell_number(int comp, int type);//count the number of cells with phenotype i in compartment comp
+        void set_Ccell_number(int type,int comp, double number); // update the cell numbers for type i in compartment comp
 
         double get_immune_count(int comp);
-
-        void set_immune_count(int comp, double number);
+		void set_immune_count(int comp, double number);
 
         /* print model data to output. */
-        void output(double dt, std::ostream & os);
+        void output(double dt, std::ostream & os);//print the output
 
     private:
+		
+		std::vector<std::vector<double>> Tumour;
+		
 
 
 };
---
+++ src/model.cpp
@@ -0,0 +1,32 @@
+#include "model.h"
+
+Model::Model(int initial_pt_size,int initial_mt_size,double num_micromets,double initial_pt_birthRate,double initial_mt_birthRate)
+{
+	int i,j;
+	j=1.0;
+	for(i=0;i<initial_pt_size;i++)
+	{
+		std::vector<double> row;
+		row.push_back(initial_pt_birthRate);//the initial proliferation size
+		row.push_back(j);//primary tumour = 1
+	   Tumour.push_back(row);
+    }
+	for(j=2.0;j<=2.0+num_micromets;j++)
+	for(i=0;i<initial_mt_size;i++)
+	{
+		std::vector<double> row;
+		row.push_back(initial_mt_birthRate);//the initial proliferation size
+		row.push_back(j);//micromet tumour compartment
+	    Tumour.push_back(row);	
+	}
+}
+
+int Model::get_Ccell_number(int comp, int type)
+{
+	int TotalTumourSize,CompTumourSize;
+	TotalTumourSize=Tumour.size();
+	int i;
+	for(i=0;i<TotalTumourSize;i++)
+		if(Tumour[i][2]==(double)comp) CompTumourSize++;
+	return CompTumourSize;
+}
